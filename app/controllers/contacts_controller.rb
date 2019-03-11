31bc76a285ac2e45329585c99a307b8509fff22a
Lesley Cheung <your.email@address.com>
2019-03-11 22:43:36 +0000

    hirb enable

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,11 +2,12 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
-  
+
+#Saves the contact in contact_params; if the contact saved... or else... show them that there was an error 
   def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-       flash[:success] = "Message sent."
+    @contact = Contact.new(contact_params) #{name: 'asdf', email: 'asdf', comments: 'asdf'}
+    if @contact.save #when you do .save, the whole thing gets saved into database 
+       flash[:success] = "Message sent." #
        redirect_to new_contact_path
     else
        flash[:danger] = @contact.errors.full_messages.join(", ")
@@ -14,6 +15,7 @@ class ContactsController < ApplicationController
     end
   end
 
+#Privatizes the contact 
   private
     def contact_params
        params.require(:contact).permit(:name, :email, :comments)
